1375fcb5c2ee99c6aa6501a41495a9c56f01693a
itchf <13014925265@163.com>
2020-05-19 03:32:47 -0700

    Add IPFSGet

--- main.go
+++ main.go
@@ -2,25 +2,51 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
 
 	shell "github.com/ipfs/go-ipfs-api"
+	"github.com/whyrusleeping/tar-utils"
 )
 
-func main() {
+var sh *shell.Shell
 
-	hash := IPFSUpload("hello")
-	fmt.Println(hash)
-	readData := IPFSCat(hash)
-	fmt.Println(readData)
+func main() {
+	sh = shell.NewShell("localhost:5001")
+
+	// upload
+	// hash := IPFSUpload("hello,world")
+	// fmt.Println(hash)
+
+	// cat
+	// readData := IPFSCat(hash)
+	// fmt.Println(readData)
+
+	// get
+	// get := IPFSGet(hash, "hash.txt")
+
 }
 
-// IPFSUpload string
+// IPFSGet Download hash content to file
+func IPFSGet(hash, outdir string) error {
+	resp, err := sh.Request("get", hash).Option("create", true).Send(context.Background())
+	if err != nil {
+		return err
+	}
+	defer resp.Close()
+	if resp.Error != nil {
+		return resp.Error
+	}
+
+	extractor := &tar.Extractor{Path: outdir}
+	return extractor.Extract(resp.Output)
+}
+
+// IPFSUpload Upload string to ipfs
 func IPFSUpload(str string) string {
-	sh := shell.NewShell("localhost:5001")
 	cid, err := sh.Add(strings.NewReader(str))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error:%s", err)
@@ -28,9 +54,8 @@ func IPFSUpload(str string) string {
 	return string(cid)
 }
 
-//IPFSCat download data
+//IPFSCat cat data in hash
 func IPFSCat(hash string) string {
-	sh := shell.NewShell("localhost:5001")
 	read, err := sh.Cat(hash)
 	if err != nil {
 		fmt.Println(err)
